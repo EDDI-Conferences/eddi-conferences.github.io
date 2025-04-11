b3b38c9d76d7659f29241807d9e456beccd0d106
spuddybike <spuddybike@gmail.com>
2025-04-11 15:43:55 +0000

    Deploying to gh-pages from @ EDDI-Conferences/eddi-conferences.github.io@5ea931920224f223a65903bff7b70ce283733951 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-past-conferences",
           title: "Past Conferences",
-          description: "",
+          description: "Previous EDDI Conferences",
           section: "Navigation",
           handler: () => {
             window.location.href = "/conferences/";
