50f7cd6a5b313df1f074df94c7cd80cc283cee5d
spuddybike <spuddybike@gmail.com>
2025-07-07 19:05:23 +0000

    Deploying to gh-pages from @ EDDI-Conferences/eddi-conferences.github.io@c6de7ca98f48b6ae47677020e4126ec925c48cd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -124,11 +124,14 @@ ninja.data = [{
           description: "9th European DDI User Conference (Lausanne)",
           section: "Conferences",handler: () => {
               window.location.href = "/eddi-2017/";
-            },},{id: "news-presentations-and-tutorials-are-now-available-on-zenodo",
-          title: 'Presentations, and tutorials are now available on Zenodo',
+            },},{id: "news-presentations-and-tutorials-from-eddi-2024-are-now-available-on-zenodo",
+          title: 'Presentations, and tutorials from EDDI 2024 are now available on Zenodo',
           description: "",
           section: "News",},{id: "news-in-2025-we-will-be-meeting-in-budapest-and-it-will-be-hosted-by-the-research-documentation-centre-at-the-hun-ren-centre-for-social-sciences-more-information",
           title: 'In 2025 we will be meeting in Budapest and it will be hosted...',
+          description: "",
+          section: "News",},{id: "news-call-for-proposals-is-now-open-we-will-be-accepting-submissions-through-1-september-2025-more-information",
+          title: 'CALL FOR PROPOSALS is now OPEN. We will be accepting submissions through 1...',
           description: "",
           section: "News",},{
         id: 'social-bluesky',
